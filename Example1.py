04896a166ebf97dcbcdbe45650b9f5cc245e560b
James Bedford <jamesbedford01@hotmail.com>
2018-06-01 15:07:58 +0100

    Merge branch 'master' of https://github.com/James-Bedford/Python_Flask.

--- Example1.py
+++ Example1.py
@@ -10,13 +10,19 @@ def start():
 def dynamic(dq):
     return "Dynamic Sysops Test"
 
-
-@app.route('/it/<int:value>')
-
+@app.route("/integer/<int:value>")
 def int_type(value):
-    print(value+1)
-    return 'Correct'
-
+    print(value + 1)
+    return "correct"
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
 
 if __name__ == '__main__':
     app.run()
