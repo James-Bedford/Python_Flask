da9577f841981384a8a9a44369de2b8fa9f46791
James Bedford <jamesbedford01@hotmail.com>
2018-06-01 15:26:55 +0100

    Merge branch 'master' of https://github.com/James-Bedford/Python_Flask.

--- Example1.py
+++ Example1.py
@@ -3,17 +3,20 @@ app = Flask(__name__)
 #add a line
 @app.route("/")
 @app.route("/start")
+
 def start():
     return "Sysops Start"
-@app.route('/dynamic/<dq>')
 
+@app.route("/integer/<int:a>")
+def int_type(a):
+    print(a + 1)
+    return "correct"
+
+
+@app.route('/dynamic/<dq>')
 def dynamic(dq):
     return "Dynamic Sysops Test"
 
-@app.route("/integer/<int:value>")
-def int_type(value):
-    print(value + 1)
-    return "correct"
 @app.route("/float/<float:value>")
 def float_type(value):
     print(value + 1)
